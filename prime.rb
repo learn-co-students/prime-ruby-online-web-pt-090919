09756a4ff262a2a35ce906b5d52b97f6030bb621
Edward Heaver <heaver.edward02@gmail.com>
2019-09-15 16:52:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(int)
+  start = 2
+  if int > 1
+    range = (start..int-1).to_a
+    range.none? do |num_to_test|
+      int % num_to_test == 0
+    end
+  else
+    false
+  end
+end
