6c1acba5fdaf93143df4b6b137f116968ef2077b
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-16 13:19:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+
+def prime?(num)
+
+  nums_arr = []
+
+  i = 1
+  while i <= num
+    nums_arr << i if num % i == 0
+    i += 1
+  end
+  num <= 0 || num == 1 || nums_arr.length > 2 ? false : true
+
+end
