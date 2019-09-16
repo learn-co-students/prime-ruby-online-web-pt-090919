99c7d77698d416b8dcfb73bc838c031ac09dd688
Srikalyan Megati <kalmegati@gmail.com>
2019-09-16 16:19:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+def prime?(number)
+  counter = 2
+  if number < 2
+    return false
+  elsif number == 2
+    return true
+  else
+    while counter < number
+      if number % counter == 0
+        return false
+      end
+      counter += 1
+    end
+  end
+  true
+end
