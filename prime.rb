1f149336b893a9fc1c1075680400120b8a7fd22f
Daniel Matney <danieljmatney@gmail.com>
2019-09-20 05:05:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  i = number - 1
+  return false if number < 2
+  until i == 1 do
+    if number % i == 0
+     return false
+    end
+    i -= 1
+  end
+  return true
+end
