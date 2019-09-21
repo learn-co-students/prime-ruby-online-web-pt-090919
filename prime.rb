84dab3c9c85ffba0864edce44c9f46d75d1df303
Jared S <jaredjames2020@gmail.com>
2019-09-21 13:17:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
+require 'pry'
 # Add  code here!
+def prime?(number)
+  counter = 2
+  if number < 2
+    return false
+  else
+    while counter < number
+      if number % counter == 0
+        return false
+      end
+      counter += 1
+    end
+  end
+  true
+end
