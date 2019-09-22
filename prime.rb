980375f56f9782c51e4b6116b4b2b324b4679d49
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 12:57:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?
+    puts(prime?(2))=(true)
+    puts(prime?(3))=(true)
+    puts(prime?(11))=(true)
+    puts(prime?(105557))=(true)
+  end
+
+def non-prime
+    puts(prime?(-1))=(false), "Be sure to account for negative numbers!"
+    puts(prime?(0))=(false)
+    puts(prime?(1))=(false)
+    puts(prime?(4))=(false)
+    puts(prime?(40))=(false)
+    puts(prime?(1763))=(false)
+    puts(prime?(101013))=(false)
+  end
+end
+
